e404318a4042d6598fdf25c7c675890e577c80fc
haohaoxing <zhangwh@sdkeji.com>
2020-09-14 17:10:47 +0800

    第一次提交

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Route from './router'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
